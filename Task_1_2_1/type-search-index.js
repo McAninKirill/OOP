a816a4f858be7fa8492d5af0a6ed4a58cd263c4d
McAninKirill <McAninKirill@users.noreply.github.com>
2023-12-06 14:02:40 +0000

    Deploying to gh-pages from @ McAninKirill/OOP@692fd880ada5810e982c402dd4f02c077ccc8972 🚀

---
+++ Task_1_2_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"BfsIterator"},{"p":"org.example","l":"DfsIterator"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"NullException"},{"p":"org.example","l":"Tree"}]
