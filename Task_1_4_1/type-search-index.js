7c8a508fa3e246c712b1e7392fbd5ff35ae22a0f
McAninKirill <McAninKirill@users.noreply.github.com>
2023-12-20 10:53:53 +0000

    Deploying to gh-pages from @ McAninKirill/OOP@0ac68ded539a35a2a90a344b88bafe187dc4a075 🚀

---
+++ Task_1_4_1/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"Grade"},{"p":"org.example","l":"GradeBook"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"Semester"}]
