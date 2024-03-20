1ae7a3f99681eb4b2ec3cfe2e3fbb8cb14217ca4
McAninKirill <McAninKirill@users.noreply.github.com>
2024-03-20 03:53:24 +0000

    Deploying to gh-pages from @ McAninKirill/OOP@59a0d941a997daae1711f22f6f437c03727d265d 🚀

--- Task_2_1_1/type-search-index.js
+++ Task_2_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example.hasnotprimecheck","l":"ConsistentlyPrime"},{"p":"org.example.hasnotprimecheck","l":"StreamPrime"},{"p":"org.example.hasnotprimecheck","l":"ThreadPrime"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example.hasnotprimecheck","l":"ConsistentlyPrime"},{"p":"org.example.plot","l":"Plot"},{"p":"org.example.hasnotprimecheck","l":"StreamPrime"},{"p":"org.example.hasnotprimecheck","l":"ThreadPrime"}]
