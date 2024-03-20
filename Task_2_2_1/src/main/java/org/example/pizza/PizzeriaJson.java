aefa9638a97d8e866a5b779f1a0040dafd9bf494
Kirill Makanin <k.makanin@g.nsu.ru>
2024-03-20 11:14:13 +0700

    fix fail

--- Task_2_2_1/src/main/java/org/example/pizza/PizzeriaJson.java
+++ Task_2_2_1/src/main/java/org/example/pizza/PizzeriaJson.java
@@ -1,10 +1,5 @@
 package org.example.pizza;
 
-public record PizzeriaJson (
-    int nBakers,
-    int mCouriers,
-    int warehouseCapacity,
-    int[] bakerSpeeds,
-    int[] courierSpeed,
-    int timeOfWork
-    ){}
+public record PizzeriaJson(int nBakers, int mCouriers, int warehouseCapacity, int[] bakerSpeeds, int[] courierSpeed,
+                           int timeOfWork) {
+}
